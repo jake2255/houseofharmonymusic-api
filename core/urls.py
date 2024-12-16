fcf34b2768abd946bbd9d1fbe3b716990d52eb9c
Ulloa, Jacob <jacobulloa@my.unt.edu>
2024-12-15 22:45:45 -0600

    modify urls

--- core/urls.py
+++ core/urls.py
@@ -8,10 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('api.urls')),
-] 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if not settings.DEBUG:
 #     urlpatterns += [
