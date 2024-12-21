ecfb183d3ce9643ea4aa3e38c73f4b0853702036
jake2255 <157094404+jake2255@users.noreply.github.com>
2024-12-20 20:24:48 -0600

    modify views and urls

--- core/urls.py
+++ core/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path('', include('api.urls')),
 ]
 
-if not settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
+# if not settings.DEBUG:
+#     urlpatterns += [
+#         re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+#     ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
