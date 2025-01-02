d458deed0b7dfb9f61f80068a3b42bf57f30f19b
jake2255 <157094404+jake2255@users.noreply.github.com>
2025-01-01 18:14:36 -0600

    modify settings

--- core/settings.py
+++ core/settings.py
@@ -172,8 +172,8 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
 # stripe payment
-STRIPE_SECRET_KEY = 'xxx'
-STRIPE_WEBHOOK_SECRET = 'xxx'
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
 
 # Static files and Media configurations
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
