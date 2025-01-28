825c708855ec335915432fe86d8cae2ccaf71bcd
jake2255 <157094404+jake2255@users.noreply.github.com>
2025-01-27 22:22:07 -0600

    change settings

--- core/settings.py
+++ core/settings.py
@@ -135,9 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'houseofharmonymusic@gmail.com'
-EMAIL_HOST_PASSWORD = 'slvu hurc zrde fnmq'
-''' App password: slvu hurc zrde fnmq '''
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
